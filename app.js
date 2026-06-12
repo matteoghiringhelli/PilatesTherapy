33e5f7791cb20a5abde617939f3597dd33cdef53
matteoghiringhelli <matteo.ghiringhelli@gmail.com>
2026-06-12 15:15:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://xxutsiiejegkgvlkgqrr.supabase.co/rest/v1";
+const SUPABASE_URL = "https://xxutsiiejegkgvlkgqrr.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dXRzaWllamVna2d2bGtncXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTUxNjgsImV4cCI6MjA5NjgzMTE2OH0.EACUhY2OGCZVswkXdygd98I0yRMT5WQz_oNeHQgdhsU";
 
 
