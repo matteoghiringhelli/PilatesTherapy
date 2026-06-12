e490febb1cacfbdb6ad7fdb126bcdbaeb2221fe3
matteoghiringhelli <matteo.ghiringhelli@gmail.com>
2026-06-12 15:09:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const SUPABASE_URL = "https://xxutsiiejegkgvlkgqrr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://xxutsiiejegkgvlkgqrr.supabase.co/rest/v1";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dXRzaWllamVna2d2bGtncXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTUxNjgsImV4cCI6MjA5NjgzMTE2OH0.EACUhY2OGCZVswkXdygd98I0yRMT5WQz_oNeHQgdhsU";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
 
 // LOGIN
 async function login() {
