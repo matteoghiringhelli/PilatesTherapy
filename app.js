d74034b7339d0f5ccca62b557f1c17407157dc2b
matteoghiringhelli <matteo.ghiringhelli@gmail.com>
2026-06-12 15:01:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+const SUPABASE_URL = "https://xxutsiiejegkgvlkgqrr.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dXRzaWllamVna2d2bGtncXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTUxNjgsImV4cCI6MjA5NjgzMTE2OH0.EACUhY2OGCZVswkXdygd98I0yRMT5WQz_oNeHQgdhsU";
 
-const supabase = supabase.createClient(
-  "https://xxutsiiejegkgvlkgqrr.supabase.co/rest/v1/",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dXRzaWllamVna2d2bGtncXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTUxNjgsImV4cCI6MjA5NjgzMTE2OH0.EACUhY2OGCZVswkXdygd98I0yRMT5WQz_oNeHQgdhsU"
-);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // LOGIN
 async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
     email,
     password,
   });
 
   if (error) {
-    alert("Errore login");
+    alert("Errore login: " + error.message);
   } else {
     window.location.href = "/dashboard.html";
   }
@@ -23,12 +22,13 @@ async function login() {
 
 // CARICA CLIENTI
 async function loadClienti() {
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from("clienti")
     .select("*");
 
   if (error) {
     console.error(error);
+    alert("Errore caricamento dati");
     return;
   }
 
