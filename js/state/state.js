4f3e77b22f3193c7b5570d790623d34811e04b85
matteoghiringhelli <matteo.ghiringhelli@gmail.com>
2026-06-26 06:31:20 +0200

    Update state.js

--- js/state/state.js
+++ js/state/state.js
@@ -3,23 +3,37 @@
 // ============================
 
 window.state = {
-
   clienti: [],
   lezioni: [],
   prenotazioni: [],
   pacchetti: [],
+  conti: [],
 
   ui: {
     paginaLezioni: 1,
     paginaPrenotazioni: 1,
+
     filtroLezioni: "tutte",
+    filtroLezioniData: "",
+
     filtroPrenotazioni: "tutte",
+    filtroPrenotazioniData: "",
+
     searchClienti: "",
-    searchPrenotazioni: ""
+    searchPrenotazioni: "",
+
+    reportPacchettiFiltro: "da_pagare",
+
+    dettaglioLezioneBoxAttivo: "dettaglioLezioneBox"
   },
 
   calendario: {
-    dataCorrente: null
-  }
+    dataCorrente: getTodayString()
+  },
 
+  charts: {
+    graficoRicaviSettimanaliInstance: null,
+    graficoRicaviMensiliTipologiaInstance: null,
+    chartFiscaleInstance: null
+  }
 };
